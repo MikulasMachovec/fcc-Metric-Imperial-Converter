39be9a3463c4dd745da880d0d32689ae2c087bf7
Mikulas Machovec <mikimachovec@gmail.com>
2025-02-04 18:17:56 +0000

    feat: convert handler handle input separation

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,14 +1,36 @@
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
     let result;
-    
+    const numRegex = /^([\d\/\.]+)/;
+    const match = input.match(numRegex);
+    if(match){
+      result=match[1];
+      if(result.includes('/')){
+        const fraction = result.split("/")
+        if (fraction.length > 2){
+          return ({ error: "Invalid number"})
+        } else {
+        result = parseFloat(fraction[0]) / parseFloat(fraction[1]);
+        }
+      } else {
+        result = parseFloat(result.replace(",","."));
+      }
+    }else{
+      result = 1 ;
+    }
     return result;
   };
   
   this.getUnit = function(input) {
     let result;
-    
+    const unitRegex = /([a-zA-Z])+$/;
+    let match = input.match(unitRegex);
+      if (match){
+        result = match[0];
+      }else{
+        result = "error"
+      }
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const expect = require('chai').expect;
+const { application } = require('express');
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', (req,res)=>{
+    const inputQuery = req.query.input;
+    const regex = /^([\d]+(?:[\/,\.][\d]+)?)([a-zA-Z]+)$/;
+    const regexUnit = /^[a-zA-Z]+$/
+    let value = convertHandler.getNum(inputQuery);
+    let unit = convertHandler.getUnit(inputQuery)
+    res.json({ value: value, unit: unit }) 
+  })
 };
